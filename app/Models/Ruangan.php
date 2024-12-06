c6826b68f3b001ee2c589f2af4e6d2dd0bf377a6
sultGG <sultgg.27@gmail.com>
2024-12-06 16:06:06 +0700

    web-inventaris-bq

---
+++ app/Models/Ruangan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ruangan extends Model
+{
+    protected  $table = 'ruangan';
+    protected $guarded;
+}
---
+++ database/migrations/2024_11_28_035735_create_ruangan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruangan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user'); //untuk pic ruangan..
+            $table->string('nama_ruangan');
+            $table->string('kode_ruangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruangan');
+    }
+};
